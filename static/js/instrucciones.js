28f145e583f58e4c9cc870a287a9394526ddc40d
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-02-28 13:01:26 -0600

    move to next page on space press

--- static/js/instrucciones.js
+++ static/js/instrucciones.js
@@ -1,54 +1,43 @@
 
-//Función para el botón de ok
-function okBtn() {
-    //Registrar acción en API
-    //*-*-*--*-*-*-*-
-    //Ir a página de instrucciones
-    window.location.href = "https://www.google.com.mx/";
+//Función para controlar presión de teclas
+function keyHandler(e, v = false, c) {
+    //Inicializamos variable para guardar código de tecla
+    var keyCode;
+
+    //IE y otros exploradores
+    if (window.event) { // IE                    
+        keyCode = e.keyCode;
+    } else if (e.which) { // Netscape/Firefox/Opera                   
+        keyCode = e.which;
+    }
+
+    //Obtenemos tecla apretada
+    var keyName = String.fromCharCode(keyCode);
+    //En caso de barra espaciadora
+    if (keyName === " ") {
+        keyName = "space";
+    }
+    //Enviamos a log
+    if (v) {
+        console.log("Key pressed:" + keyName);
+    }
+    //Si hay callback
+    if (c) {
+        c(keyName);
+    } else {
+        return keyName;
+    }
 }
 
-//Función para el botón de saber más
-function infoBtn() {
-    //Mostrar el PopUp
-    $("#PopUp").show();
-}
-
-//Función para el botón de cancelar
-function noBtn() {
-    //Registrar acción en API
-    //*-*-*--*-*-*-*-
-    //Ir a Google
-    window.location.href = "https://www.google.com.mx/";
-}
-
-//Función para el botón de cerrar popup
-function closePopUpBtn() {
-    //Cerrar el PopUp
-    $("#PopUp").hide();
-}
-
-//Función para configurar botones
-function setBtns() {
-    //Botón ok
-    $("#OkButton").click(okBtn);
-    //Botón para mayor información
-    $("#infoButton").click(infoBtn);
-    //Botón para cancelar
-    $("#NoButton").click(noBtn);
-    //Botón que cierra el PopUp
-    $("#PopUpClose").click(closePopUpBtn);
-    //Si el usuario da click en cualquier lugar de la ventana, cerramos el PopUp
-    $("#PopUp").click(closePopUpBtn).children().click(function (e) {
-        return false;
-    })
+function keyCallBack(keyName) {
+    if (keyName == "space") {
+        window.location.href = "/IAT/static/src/iat.php";
+    }
 }
 
 //Cuando el documento se carga
 $(document).ready(function () {
-    //Configuramos botones
-    setBtns();
-    //Inicializamos conector de API
-    myAPI = new iatAPI()
-    //Obtenemos nuevo usuario
-    myAPI.endPoints.POST_newUser();
+    $(document).keypress(function (e) {
+        keyHandler(e, false, keyCallBack);
+    }); 
 });
