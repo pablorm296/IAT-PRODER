4664bf39a24c6d503df636f021755145710c857d
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-02-27 23:28:37 -0600

    relative path issue fixed

--- static/src/welcomeHeader.php
+++ static/src/welcomeHeader.php
@@ -10,7 +10,7 @@
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!-- JS -->
-    <script src="./js/api/api.min.js"></script>
-    <script src="./js/welcome.min.js"></script>
+    <script src="../js/api/api.min.js"></script>
+    <script src="../js/welcome.min.js"></script>
     <!-- Título -->
     <title>Prueba de Asociaciones Implíticas | El Colegio de México</title>
