bb30f86f2f5ddb99c9925a57929eceb3a26c8e67
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-02-28 12:14:09 -0600

    instrucciones js added

---
+++ static/js/instrucciones.js
@@ -0,0 +1,54 @@
+
+//Función para el botón de ok
+function okBtn() {
+    //Registrar acción en API
+    //*-*-*--*-*-*-*-
+    //Ir a página de instrucciones
+    window.location.href = "https://www.google.com.mx/";
+}
+
+//Función para el botón de saber más
+function infoBtn() {
+    //Mostrar el PopUp
+    $("#PopUp").show();
+}
+
+//Función para el botón de cancelar
+function noBtn() {
+    //Registrar acción en API
+    //*-*-*--*-*-*-*-
+    //Ir a Google
+    window.location.href = "https://www.google.com.mx/";
+}
+
+//Función para el botón de cerrar popup
+function closePopUpBtn() {
+    //Cerrar el PopUp
+    $("#PopUp").hide();
+}
+
+//Función para configurar botones
+function setBtns() {
+    //Botón ok
+    $("#OkButton").click(okBtn);
+    //Botón para mayor información
+    $("#infoButton").click(infoBtn);
+    //Botón para cancelar
+    $("#NoButton").click(noBtn);
+    //Botón que cierra el PopUp
+    $("#PopUpClose").click(closePopUpBtn);
+    //Si el usuario da click en cualquier lugar de la ventana, cerramos el PopUp
+    $("#PopUp").click(closePopUpBtn).children().click(function (e) {
+        return false;
+    })
+}
+
+//Cuando el documento se carga
+$(document).ready(function () {
+    //Configuramos botones
+    setBtns();
+    //Inicializamos conector de API
+    myAPI = new iatAPI()
+    //Obtenemos nuevo usuario
+    myAPI.endPoints.POST_newUser();
+});
