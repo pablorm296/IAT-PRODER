9682920ba4a095abd426527ce4531bf00fc176c2
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-03-03 12:55:26 -0600

    main IAT page added

---
+++ static/src/iatHeader.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Estilos -->
+    <link rel="stylesheet" href="../style/iat.css">
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- JS -->
+    <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
+    <script src="../js/iat.min.js"></script>
+    <script src="../js/api/api.min.js"></script>
+    <!-- Título -->
+    <title>Prueba de Asociaciones Implíticas | El Colegio de México</title>
