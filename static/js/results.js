376da7a11ae5afeb83572f8ad64672612e4186fd
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-03-12 13:08:08 -0600

    stats now displayed in table

--- static/js/results.js
+++ static/js/results.js
@@ -16,9 +16,31 @@ function loadResults() {
         const slowestLatency = responseContent.slowestLatency;
         const meanLatency = responseContent.meanLatency;
         const errorCount = responseContent.errorCount;
-        //Colocamos los resiltados en la página
+        //Colocamos los resultados en la página
         __iatScore = iatScore;
         $("#result_iat_score").text(iatScore);
+        $("#stat_mean_latency").text(meanLatency);
+        $("#stat_min_latency").text(fastestLatency);
+        $("#stat_max_latency").text(slowestLatency);
+        $("#stat_errors").text(errorCount);
+
+        //Dependiendo de la fortaleza de los resultados
+        if (iatScore <= 0.2 || iatScore >= -0.2 ) {
+            $("#result_preference_size").text("no tienes preferencia alguna");
+            $("#result_preference_target").text("las personas de piel oscura o piel clara");
+        } else if (iatScore >= 0.5) {
+            $("#result_preference_size").text("tienes una ligera preferencia");
+            $("#result_preference_target").text("las personas de piel oscura");
+        } else if (iatScore >= 0.8) {
+            $("#result_preference_size").text("tienes una fuerte preferencia");
+            $("#result_preference_target").text("las personas de piel oscura");
+        } else if (iatScore <= -0.5) {
+            $("#result_preference_size").text("tienes una ligera preferencia");
+            $("#result_preference_target").text("las personas de piel clara");
+        } else if (iatScore <= -0.8) {
+            $("#result_preference_size").text("tienes una fuerte preferencia");
+            $("#result_preference_target").text("las personas de piel clara");
+        }
     });
 }
 
