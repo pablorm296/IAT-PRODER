07d079ac46b3e24234821ab6bccca5884490e16f
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-03-12 01:11:33 -0600

    results php page and header added

---
+++ static/src/results.php
@@ -0,0 +1,10 @@
+<?php
+//Cambiamos de directorio
+chdir("../");
+
+//Incluimos la cabecera
+require_once('./src/resultsHeader.php');
+
+//Incluimos el contenido de la página
+require_once('./html/results.html');
+?>
