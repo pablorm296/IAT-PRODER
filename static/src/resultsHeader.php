cba7d66af19bb87786502a1a1fa682b5f4755d8e
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-03-12 19:22:20 -0600

    added Highcharts modules

--- static/src/resultsHeader.php
+++ static/src/resultsHeader.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +11,8 @@
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/highcharts-more.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <!-- JS -->
     <script src="../js/api/api.min.js"></script>
     <script src="../js/results.min.js"></script>
