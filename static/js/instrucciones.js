14002992087617d7581949cd1c5efea5839b0d2d
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-03-14 17:23:36 -0600

    examples in instructions page

--- static/js/instrucciones.js
+++ static/js/instrucciones.js
@@ -1,3 +1,5 @@
+const __stage = 0;
+var myAPI;
 
 //Función para controlar presión de teclas
 function keyHandler(e, v = false, c) {
@@ -29,16 +31,55 @@ function keyHandler(e, v = false, c) {
     }
 }
 
+//Función callback para las teclas
 function keyCallBack(keyName) {
     if (keyName == "space") {
         window.location.href = "/IAT/static/src/iat.php";
     }
 }
 
+//Colocar ejemplos
+function putExamples(data) {
+    //Obtener catgeorías de ejemplos
+    const goodWords, badWords, whitePeople, darkPeope
+    goodWords = data.good;
+    badWords = data.bad;
+    whitePeople = data.white;
+    darkPeope = data.dark;
+    //Ponemos los ejemplos
+    //Palabras buenas
+    for (let index = 0; index < goodWords.length; index++) {
+        //Obtenemos la palabra
+        const word = goodWords[index];
+        //La agregamos a la lista de palabras
+        $("#good_examples").append(word);
+        //Si no es la última palabra
+        if (index < goodWords.length - 1) {
+            $("#good_examples").append(",");
+        }
+    }
+    //Palabras malas
+    for (let index = 0; index < badWords.length; index++) {
+        //Obtenemos la palabra
+        const word = badWords[index];
+        //La agregamos a la lista de palabras
+        $("#good_examples").append(word);
+        //Si no es la última palabra
+        if (index < badWords.length - 1) {
+            $("#good_examples").append(",");
+        }
+    }
+}
+
 //Cuando el documento se carga
 $(document).ready(function () {
+    //Asignamos handler y key de las teclas
     $(document).keypress(function (e) {
         e.preventDefault();
         keyHandler(e, false, keyCallBack);
-    }); 
+    });
+    //Lllamamos los estímulos necesarios
+    //Nueva instancia de myAPI
+    myAPI = new iatAPI()
+    myAPI.endPoints.GET_stimuli(__stage, putExamples);
 });
