e291e5a49b3f3fa72463a1a499034cf59c0569a5
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-04-14 17:32:18 -0500

    moved full screen event to welcolme page

--- static/src/iatHeader.php
+++ static/src/iatHeader.php
@@ -10,7 +10,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!-- JS -->
     <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/screenfull.js/5.0.0/screenfull.min.js" integrity="sha256-syuFHf7mRNoTlsx0tObV2rxlKzXoz4kfNuPo8f3pSiU=" crossorigin="anonymous"></script>
     <script src="../js/iat.min.js"></script>
     <script src="../js/api/api.min.js"></script>
     <!-- Título -->
