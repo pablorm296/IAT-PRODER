9a7799fef60a3eaa06e103eafe7f37f5469a7603
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-07-14 15:56:39 -0500

    Load configuration and blueprints

--- Src/IAT/main.py
+++ Src/IAT/main.py
@@ -1,10 +1,26 @@
 from flask import Flask
 
+from IAT.Config import Reader
+from IAT.Views import Front
+
 def create_app(debug = True):
+    # Read configuration
+    if debug:
+        myConfigReader = Reader(path = "Debug", load = "app")
+    else:
+        myConfigReader = Reader(path = None, load = "app")
+
+    CONFIG = myConfigReader.Config
+
     # Create a new app
-    app = Flask(__name__, static_folder = "/Static", template_folder = "/Templates", instance_relative_config = True)
+    app = Flask(__name__, static_folder = "Static", template_folder = "Templates", instance_relative_config = True)
     # Define app key
     if debug:
         app.secret_key = "DEBUG_KEY"
     else:
-        pass
+        app.secret_key = myConfigReader.Config["app"]["secret_key"].encode()
+
+    # Register blueprints
+    app.register_blueprint(Front)
+
+    return app
