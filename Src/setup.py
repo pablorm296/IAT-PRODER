f089037a8e9ee8cc5ed31108726ed909ed42b790
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-07-14 12:24:34 -0500

    Defined setup file

---
+++ Src/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+# Load README
+with open("README.md") as readmeFile:
+    long_description = readmeFile.read()
+
+setuptools.setup(
+    name = "IAT_PRODER",
+    version = "0.1",
+    author = "Pablo Reyes Moctezuma",
+    author_email = "pablo.reyes.moctezuma@gmail.com",
+    description = "A lightweight IAT for measeuring stereotypes on skin tone",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    packages = ["IAT"],
+    package_dir = {
+        "IAT": "."
+    }
+)
