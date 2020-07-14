fe0256d1ea50155e6ca9dcf0f9e2723d75fc70c4
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-07-14 15:57:20 -0500

    Fixed bugs that crashed config reading, and added Mongo_Uri property

--- Src/IAT/Config/Reader.py
+++ Src/IAT/Config/Reader.py
@@ -79,25 +79,49 @@ class Reader:
                         self.Config.update(keyName = parsed)
                         logger.debug("{0} loaded successfully".format(tmp_target))
 
-                    # Check if config files could be loaded
-                    if self.Config.get("app", None) is None or self.Config.get("stimuli", None) is None or self.Config.get("text", None) is None:
-                        error_msg = "It seems that no configuration file could be loaded. Please make sure all config files are properly installed in /etc/Iat"
-                        logger.error(error_msg)
-                        raise Exception(error_msg)
             else:
                 # Define file path
                 tmp_target = "{0}/{1}.config.json".format(path, load)
                 # Check if it exists
                 logger.debug("Checking if {0} exists...".format(tmp_target))
                 if not os.path.exists(tmp_target):
-                    error_msg = "Could not find {0}. Please make sure all config files are properly installed in /etc/Iat".format(tmp_target)
-                    logger.error(error_msg)
-                    raise FileNotFoundError(error_msg)
+                    if len(self.__paths) == 1:
+                        error_msg = "Could not find {0}. Please make sure all config files are properly installed in /etc/Iat".format(tmp_target)
+                        logger.error(error_msg)
+                        raise FileNotFoundError(error_msg)
+                    else:
+                        warning_msg = "Could not find {0}".format(tmp_target)
+                        logger.warning(warning_msg)
+                        continue
                 # Open file if exists
                 logger.debug("Reading {0}...".format(tmp_target))
                 with open(tmp_target) as file:
                     # Read
                     parsed = json.load(file)
                     # Update config dict
-                    self.Config.update(load = parsed)
+                    self.Config[load] = parsed
                     logger.debug("{0} loaded successfully".format(tmp_target))
+
+        # Check if config files could be loaded
+        if self.Config.get("app", None) is None and self.Config.get("stimuli", None) is None and self.Config.get("text", None) is None:
+            error_msg = "It seems that no configuration file could be loaded. Please make sure all config files are properly installed in /etc/Iat"
+            logger.error(error_msg)
+            raise Exception(error_msg)
+
+    @property
+    def Mongo_Uri(self):
+        # If there is not any app config loaded, return none
+        if self.Config.get("app", None):
+            warning_msg = "It seems that there is not an app config loaded"
+            logger.warning(warning_msg)
+            return None
+        else:
+            #Make Mongo_Uri
+            uri = "mongodb://{0}:{1}@{2}:{3}/{4}".format(
+                self.Config["app"]["mongo_user"],
+                self.Config["app"]["mongo_password"],
+                self.Config["app"]["mongo_host"],
+                self.Config["app"]["mongo_port"],
+                self.Config["app"]["mongo_db_name"]
+            )
+            return uri
