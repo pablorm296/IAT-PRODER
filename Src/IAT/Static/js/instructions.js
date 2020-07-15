988660c0d1435a4da982069b820e7b01405acc06
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-07-14 22:51:43 -0500

    Better comments, removed API call, and minify

--- Src/IAT/Static/js/instructions.js
+++ Src/IAT/Static/js/instructions.js
@@ -1,29 +1,22 @@
-const __stage = 0;
-var myAPI;
-
-//Función para controlar presión de teclas
+// Main KeyHandler
 function keyHandler(e, v = false, c) {
-    //Inicializamos variable para guardar código de tecla
+    // Init an empty where key code is going to be stored
     var keyCode;
 
-    //IE y otros exploradores
+    // Get keycode
     if (window.event) { // IE                    
         keyCode = e.keyCode;
     } else if (e.which) { // Netscape/Firefox/Opera                   
         keyCode = e.which;
     }
 
-    //Obtenemos tecla apretada
+    // Get name of pressed key
     var keyName = String.fromCharCode(keyCode);
-    //En caso de barra espaciadora
+    // If it's a blank space, it means use pressed space key
     if (keyName === " ") {
         keyName = "space";
     }
-    //Enviamos a log
-    if (v) {
-        console.log("Key pressed:" + keyName);
-    }
-    //Si hay callback
+    // If there's a callback, send. Else, return keyName
     if (c) {
         c(keyName);
     } else {
@@ -31,70 +24,18 @@ function keyHandler(e, v = false, c) {
     }
 }
 
-//Función callback para las teclas
+// Key Pressed Callback
 function keyCallBack(keyName) {
     if (keyName == "space") {
         window.location.href = "/iat";
     }
 }
 
-//Colocar ejemplos
-function putExamples(data) {
-    const responseContent = data.responseContent;
-
-    //Obtener catgeorías de ejemplos
-    const goodWords = responseContent.good;
-    const badWords = responseContent.bad;
-    const whitePeople = responseContent.white;
-    const darkPeope = responseContent.dark;
-    //Ponemos los ejemplos
-    //Palabras buenas
-    for (let index = 0; index < goodWords.length; index++) {
-        //Obtenemos la palabra
-        const word = goodWords[index];
-        //La agregamos a la lista de palabras
-        $("#good_examples").append(word.content);
-        //Si no es la última palabra
-        if (index < goodWords.length - 1) {
-            $("#good_examples").append(", ");
-        }
-    }
-    //Palabras malas
-    for (let index = 0; index < badWords.length; index++) {
-        //Obtenemos la palabra
-        const word = badWords[index];
-        //La agregamos a la lista de palabras
-        $("#bad_examples").append(word.content);
-        //Si no es la última palabra
-        if (index < badWords.length - 1) {
-            $("#bad_examples").append(", ");
-        }
-    }
-    //Personas de piel clara
-    for (let index = 0; index < whitePeople.length; index++) {
-        //Obtenemos la palabra
-        const img = whitePeople[index];
-        //La agregamos a la lista de personas
-        $("#white_examples").append(`<img class="example" src='../imgs/stimuli/${img.content}.JPG'>`);
-    }
-    //Personas de piel oscura
-    for (let index = 0; index < darkPeope.length; index++) {
-        //Obtenemos la palabra
-        const img = darkPeope[index];
-        //La agregamos a la lista de personas
-        $("#dark_examples").append(`<img class="example" src='../imgs/stimuli/${img.content}.JPG'>`);
-    }
-}
-
-//Cuando el documento se carga
+// When document is loaded
 $(document).ready(function () {
-    //Asignamos handler y key de las teclas
+    // Assign keyHandler to key press event
     $(document).keypress(function (e) {
         e.preventDefault();
         keyHandler(e, false, keyCallBack);
     });
-    //Lllamamos los estímulos necesarios
-    //Nueva instancia de myAPI
-    myAPI = new iatAPI()
-    myAPI.endPoints.GET_stimuli(__stage, putExamples);
 });
