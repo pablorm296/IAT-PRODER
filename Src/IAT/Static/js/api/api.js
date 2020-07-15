483e1258be26ac732707e5f37f82e82f1fe30bba
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-07-15 01:51:56 -0500

    On Api Error, redirect to discriminacion.colmex.mx

--- Src/IAT/Static/js/api/api.js
+++ Src/IAT/Static/js/api/api.js
@@ -10,7 +10,7 @@ class iatAPI {
                     dataType: "json",
                     data: JSON.stringify(payload),
                     async: false,
-                    error: function() {alert("An unexpected error occurred when updating the test results! Please contact server admin")}
+                    error: function() {alert("An unexpected error occurred when updating the test results! Please contact server admin"); window.location.href = "discriminacion.colmex.mx/";}
                 });
             },
             GET_results: function (success) {
@@ -21,7 +21,7 @@ class iatAPI {
                     dataType: "json",
                     async: false,
                     success: success,
-                    error: function() {alert("An unexpected error occurred when getting the test results! Please contact server admin")}
+                    error: function() {alert("An unexpected error occurred when getting the test results! Please contact server admin"); window.location.href = "discriminacion.colmex.mx/";}
                 });
             },
             GET_stimuli: function (stage, success) {
@@ -30,7 +30,7 @@ class iatAPI {
                     url: `api/stimuli?stage=${stage}`,
                     dataType: "json",
                     success: success,
-                    error: function() {alert("An unexpected error occurred when getting the test stimuli! Please contact server admin")}
+                    error: function() {alert("An unexpected error occurred when getting the test stimuli! Please contact server admin"); window.location.href = "discriminacion.colmex.mx/";}
                 });
             }
         }
