cb73c331f9ab736a40bb1078395df3be9270a1cf
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-07-14 23:13:36 -0500

    Make cookie session last for one year

--- Src/IAT/main.py
+++ Src/IAT/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from datetime import timedelta
 import os
+import flask
 
 from IAT.Config import Reader
 from IAT.Views import Front
@@ -19,13 +21,20 @@ def create_app(debug = None):
 
     # Create a new app
     app = Flask(__name__, static_folder = "Static", template_folder = "Templates", instance_relative_config = True)
+
     # Define app key
     if debug:
         app.secret_key = "DEBUG_KEY"
     else:
         app.secret_key = myConfigReader.Config["app"]["secret_key"].encode()
-
+        
     # Register blueprints
     app.register_blueprint(Front)
+
+    # Set session cokie expiration
+    @app.before_request
+    def make_session_permanent():
+        flask.session.permanent = True
+        app.permanent_session_lifetime = timedelta(days = 365)
 
     return app
