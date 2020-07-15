c13083d0a753a2d1c1909c0e0631478384c4b23c
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-07-15 01:47:41 -0500

    Added error messages on Api requests

--- Src/IAT/Static/js/api/api.js
+++ Src/IAT/Static/js/api/api.js
@@ -9,10 +9,11 @@ class iatAPI {
                     contentType: 'application/json',
                     dataType: "json",
                     data: JSON.stringify(payload),
-                    async: false
+                    async: false,
+                    error: function() {alert("An unexpected error occurred when updating the test results! Please contact server admin")}
                 });
             },
-            GET_results: function (success, error) {
+            GET_results: function (success) {
                 $.ajax({
                     type: "GET",
                     url: "api/result",
@@ -20,15 +21,16 @@ class iatAPI {
                     dataType: "json",
                     async: false,
                     success: success,
-                    error: error
+                    error: function() {alert("An unexpected error occurred when getting the test results! Please contact server admin")}
                 });
             },
-            GET_stimuli: function (stage, success, error) {
+            GET_stimuli: function (stage, success) {
                 $.ajax({
                     type: "GET",
                     url: `api/stimuli?stage=${stage}`,
                     dataType: "json",
-                    success: success
+                    success: success,
+                    error: function() {alert("An unexpected error occurred when getting the test stimuli! Please contact server admin")}
                 });
             }
         }
