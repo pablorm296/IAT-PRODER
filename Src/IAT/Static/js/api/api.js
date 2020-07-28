4d010db281292390ec075a8969b554db8261603c
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-07-28 17:48:01 -0500

    Fixed API routes

--- Src/IAT/Static/js/api/api.js
+++ Src/IAT/Static/js/api/api.js
@@ -5,7 +5,7 @@ class iatAPI {
             POST_results: function (payload) {
                 $.ajax({
                     type: "POST",
-                    url: "api/result",
+                    url: "api/iat/results",
                     contentType: 'application/json',
                     dataType: "json",
                     data: JSON.stringify(payload),
@@ -24,20 +24,10 @@ class iatAPI {
                     error: function() {alert("An unexpected error occurred when posting the survey results! Please contact server admin"); window.location.href = "https://discriminacion.colmex.mx/";}
                 });
             },
-            GET_results: function (success) {
-                $.ajax({
-                    type: "GET",
-                    url: "api/iat/results",
-                    contentType: 'application/json',
-                    dataType: "json",
-                    success: success,
-                    error: function() {alert("An unexpected error occurred when getting the test results! Please contact server admin"); window.location.href = "https://discriminacion.colmex.mx/";}
-                });
-            },
             GET_stimuli: function (stage, success) {
                 $.ajax({
                     type: "GET",
-                    url: `api/stimuli?stage=${stage}`,
+                    url: `api/iat/stimuli?stage=${stage}`,
                     dataType: "json",
                     success: success,
                     error: function() {alert("An unexpected error occurred when getting the test stimuli! Please contact server admin"); window.location.href = "https://discriminacion.colmex.mx/";}
