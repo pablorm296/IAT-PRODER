f68fed0316a3a3b9adf5d66802ed363eb28d7e3e
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-08-24 02:30:24 -0500

    Changed Env path

--- Docker/IAT/bin/restartServices.sh
+++ Docker/IAT/bin/restartServices.sh
@@ -6,7 +6,7 @@ git clone https://github.com/pablorm296/IAT-PRODER.git /srv/IAT
 
 # Make a virtual env
 pip3 install virtualenv
-virtualenv /srv/IAT/env
+virtualenv /srv/IAT/Env
 
 # Install requirements
- /bin/bash -c "source /srv/IAT/env/bin/activate && pip install -r /srv/IAT/requirements.txt && pip install -e /srv/IAT/Src"
+ /bin/bash -c "source /srv/IAT/Env/bin/activate && pip install -r /srv/IAT/requirements.txt && pip install -e /srv/IAT/Src"
