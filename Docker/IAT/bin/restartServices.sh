6ba6211b43cb504db37c548f7d8b0dd31087682b
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-08-24 02:15:02 -0500

    Changed git source from ssh to https and install virtual env

--- Docker/IAT/bin/restartServices.sh
+++ Docker/IAT/bin/restartServices.sh
@@ -2,12 +2,11 @@
 service nginx restart
 
 # Copy repo
-git clone git@github.com:pablorm296/IAT-PRODER.git /srv/IAT
+git clone https://github.com/pablorm296/IAT-PRODER.git /srv/IAT
 
 # Make a virtual env
+pip3 install virtualenv
 virtualenv /srv/IAT/env
 
 # Install requirements
-source /srv/IAT/env/bin/activate
-
-pip install -r /srv/IAT/requirements.txt
+ /bin/bash -c "source srv/IAT/env/bin/activate && pip install -r /srv/IAT/requirements.txt"
